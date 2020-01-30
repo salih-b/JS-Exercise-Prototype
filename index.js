ce3cd403badeccfee7318b1b54693d20c1e472e8
Salih Babekr <salihmorta17@gmail.com>
2020-01-30 10:27:47 -0500

    task one and two completed

--- index.js
+++ index.js
@@ -39,27 +39,55 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+this.stomach = [];
+this.age = age;
+this.name = name;
 }
+Person.prototype.eat = function(someFood){
+  if (this.stomach.length < 10){
+    this.stomach.push(someFood);
+  }
+}
+Person.prototype.poop = function(){
+  return this.stomach = [];
+}
+Person.prototype.toString = function(){
+  // let ageString = function(){
+  //   this.age.toString()
+  // }
+  return `${this.name}, ${this.age.toString()}`;
+}
+
 
 /*
   TASK 2
-    - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
+    - Write a Car constructor that initializes `model` and `milesPerGallon` from 
+    arguments.
     - All instances built with Car:
         + should initialize with an `tank` at 0
         + should initialize with an `odometer` at 0
-    - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
+    - Give cars the ability to get fueled with a `.fill(gallons)` method. 
+    Add the gallons to `tank`.
     - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
         + Should cause the `odometer` to go up.
         + Should cause the the `tank` to go down taking `milesPerGallon` into account.
-    - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
-        + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
+    - STRETCH: A car which runs out of `fuel` while driving can't drive any more 
+    distance:
+        + The `drive` method should return a string 
+        "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+this.model = model;
+this.milesPerGallon = milesPerGallon;
+this.tank = 0;
+this.odometer = 0;
 }
+Car.prototype.fill = function(gallons){
+  this.tank =+ gallons;
+} 
+
 
 /*
   TASK 3
